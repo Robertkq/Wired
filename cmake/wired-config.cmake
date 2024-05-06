8dd38484f6049ea14bf8abe6b0f635f4c8e3e7b7
Robertkq <robertvuia06@gmail.com>
2024-05-06 19:54:18 +0300

    Added asio + fix clang

--- cmake/wired-config.cmake
+++ cmake/wired-config.cmake
@@ -1,8 +1,8 @@
 file(GLOB_RECURSE HEADERS "include/*.h")
 
-find_package(asio REQUIRED)
+add_library(Wired INTERFACE ${HEADERS})
 
 target_include_directories(Wired INTERFACE ${CMAKE_CURRENT_LIST_DIR}/../include)
-target_include_directories(Wired INTERFACE ${CMAKE_CURRENT_LIST_DIR}/../vendor/asio/asio/include)
+target_include_directories(Wired INTERFACE ${CMAKE_CURRENT_LIST_DIR}/../include/asio)
 
 message("Sucessfully added Wired library to project.")
