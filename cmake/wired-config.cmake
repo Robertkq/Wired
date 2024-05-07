75941d7c4e0276e9adc43832b2c0d017b1e3948f
Robert <34575005+Robertkq@users.noreply.github.com>
2024-05-07 20:57:37 +0300

    Add asio in Wired package (#3)
    * remove boost asio
    
    * Adding chris' asio in vendor/
    
    * Added asio + fix clang
    
    * Successfully added & compiled asio from wired package
    
    * checkout with recursive submodules
    
    * Some definitions for client interface

--- cmake/wired-config.cmake
+++ cmake/wired-config.cmake
@@ -3,5 +3,6 @@ file(GLOB_RECURSE HEADERS "include/*.h")
 add_library(Wired INTERFACE ${HEADERS})
 
 target_include_directories(Wired INTERFACE ${CMAKE_CURRENT_LIST_DIR}/../include)
+target_include_directories(Wired INTERFACE ${CMAKE_CURRENT_LIST_DIR}/../include/asio)
 
 message("Sucessfully added Wired library to project.")
--- include/wired/client.h
+++ include/wired/client.h
@@ -1,12 +1,43 @@
-#ifndef CLIENT_H
-#define CLIENT_H
+#ifndef WIRED_CLIENT_H
+#define WIRED_CLIENT_H
 
-class Client {
-public:
-  int hello;
+#include <wired/types.h>
 
-private:
-  // Private member variables and methods
+#include <asio.hpp>
+#include <iostream>
+
+namespace wired {
+template <typename T>
+class client_interface {
+  public:
+    client_interface();
+    client_interface(const client_interface& other) = delete;
+    client_interface(client_interface&& other);
+    virtual ~client_interface();
+
+    client_interface& operator=(const client_interface&& other) = delete;
+    client_interface& operator=(client_interface&& other);
+
+    bool connect(const std::string& host, const std::string& port,
+                 connection_strategy strategy = connection_strategy::none);
+
+  private:
 };
 
-#endif // CLIENT_H
+template <typename T>
+client_interface<T>::client_interface() {}
+
+template <typename T>
+client_interface<T>::client_interface(client_interface&& other) {}
+
+template <typename T>
+client_interface<T>::~client_interface() {}
+
+template <typename T>
+client_interface<T>& client_interface<T>::operator=(client_interface&& other) {}
+
+} // namespace wired
+
+#endif // WIRED_CLIENT_H
+
+} // namespace wired
