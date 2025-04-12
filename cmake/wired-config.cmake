dc6cbf6a154a782ecaf5bb6a3c9c9061bff415b2
Robertkq <robertvuia06@gmail.com>
2025-04-12 19:09:34 +0300

    quick fix for install prefix

--- cmake/wired-config.cmake
+++ cmake/wired-config.cmake
@@ -1,7 +1,6 @@
 set(Wired_INCLUDE_DIRS 
-"${CMAKE_INSTALL_PREFIX}/wired/include"
-"${CMAKE_INSTALL_PREFIX}/wired/include/asio/include/"
+"${CMAKE_CURRENT_LIST_DIR}/../wired/include"
+"${CMAKE_CURRENT_LIST_DIR}/../wired/include/asio/include/"
 )
 
-message("wired-config.cmake successfully generated")
 
